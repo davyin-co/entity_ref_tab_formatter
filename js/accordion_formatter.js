d461becab125c6720273deffc810ac83fb4c72ee
rakeshjames <rakeshjames@1177822.no-reply.drupal.org>
2016-10-26 13:05:30 +0530

    Issue #2822189 by rakesh.gectcr, manojapare: Accordion should collapsed while loading the page

--- js/accordion_formatter.js
+++ js/accordion_formatter.js
@@ -1,5 +1,5 @@
 (function ($, Drupal, drupalSettings) {
  $( ".entity-ref-tab-formatter-accordion" ).accordion({
-  collapsible: true
+  collapsible: true, active: false
 });
 })(jQuery, Drupal, drupalSettings);
